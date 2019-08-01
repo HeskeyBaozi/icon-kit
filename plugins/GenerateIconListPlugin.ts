2593937b71587ec12b66a2cc81d027869a25e363
HeskeyBaozi <hezhiyu233@foxmail.com>
2019-08-01 17:40:08 +0800

    fix twotone

--- plugins/GenerateIconListPlugin.ts
+++ plugins/GenerateIconListPlugin.ts
@@ -1,8 +1,9 @@
 import { KitPlugin, ProxyPluginAPI, Asset } from '@kit';
-import { set } from 'lodash';
+import { set, get, template } from 'lodash';
 import { getThemeAccordingToDir } from '../utils';
 import GenerateFilesPlugin from './GenerateFilesPlugin';
-import { resolve } from 'path';
+import { resolve, parse, relative } from 'path';
+import { readFileSync } from 'fs-extra';
 
 export interface GenerateIconListPluginOptions {
   output: string;
@@ -18,31 +19,43 @@ export default class GenerateIconListPlugin implements KitPlugin {
     const acc: {
       [name: string]: {
         fill?: string;
-        outlint?: string;
-        'two-tone'?: string;
+        outline?: string;
+        'twotone'?: string;
       };
     } = {};
 
     api.syncHooks.beforeEmit.tap(this.namespace, (asset: Asset) => {
       if (asset.from && asset.from.dir) {
         const theme = getThemeAccordingToDir(asset.from.dir);
-        const absolute = asset.from.absolute;
+        const rl = relative(this.options.output, asset.from.absolute);
         const name = asset.from.name;
-        set(acc, [name, theme], `![${name}](${absolute})`);
+        set(acc, [name, theme], `![${name}](${rl})`);
       }
     });
 
-    api.registerPlugin(
-      new GenerateFilesPlugin([
-        {
-          output: this.options.output,
-          dataSource: resolve(__dirname, '../templates/icons-list.md')
-        }
-      ])
-    );
-
     api.syncHooks.beforeExtraAssetsTakingEffect.tap(this.namespace, () => {
-      console.log(acc);
+      let content = '';
+      Object.keys(acc).forEach((name) => {
+        const target = acc[name]!;
+        const row = ['fill', 'outline', 'twotone'].map((theme) =>
+          get(target, theme, ' - ')
+        );
+        row.unshift(name);
+        content += row.join(' | ') + '\n';
+      });
+
+      const tpl = readFileSync(
+        resolve(__dirname, '../templates/icons-list.md'),
+        'utf8'
+      );
+
+      api.extraAssets$.next({
+        to: {
+          ...parse(this.options.output),
+          absolute: this.options.output
+        },
+        content: template(tpl)({ icons: content })
+      });
     });
   }
 }
